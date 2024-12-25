faea1449488238c1f4ecfc4b662ad7d4ac9772a5
imesh <imesh.nimsitha@gmail.ca>
2024-12-25 01:56:51 -0500

    created home page with password protection

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <></>;
+import { PasswordPrompt } from "@/components/PasswordPrompt";
+import { cookies } from "next/headers";
+
+export default async function Home() {
+  const cookieStore = await cookies();
+  const loginCookie = cookieStore.get(process.env.PASSWORD_COOKIE!);
+  const isLoggedIn = !!loginCookie?.value;
+
+  if (!isLoggedIn) {
+    return <PasswordPrompt />;
+  }
+
+  return (
+    <div>
+      <p>Logged in.</p>
+    </div>
+  );
 }
