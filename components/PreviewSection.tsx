872849db4541a0686d3335faa5ad9ec6e2517c1b
imesh <imesh.nimsitha@gmail.ca>
2024-12-28 23:22:18 -0500

    added blur fade animation

--- components/PreviewSection.tsx
+++ components/PreviewSection.tsx
@@ -1,13 +1,16 @@
+import BlurFade from "./ui/blur-fade";
 import Safari from "./ui/safari";
 
 export default function PreviewSection() {
   return (
     <div className="relative w-full max-w-5xl mx-auto pb-20" id="product">
-      <Safari
-        url="affinity.ai"
-        className="size-full"
-        imageSrc="https://via.placeholder.com/1200x750"
-      />
+      <BlurFade inView inViewMargin="-20px" delay={0.2} direction="up">
+        <Safari
+          url="affinity.ai"
+          className="size-full"
+          imageSrc="https://via.placeholder.com/1200x750"
+        />
+      </BlurFade>
     </div>
   );
 }
