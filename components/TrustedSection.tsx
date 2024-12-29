e5132fdc5a6137d4faae69f84497c708d3d2fb88
imesh <imesh.nimsitha@gmail.ca>
2024-12-28 20:31:55 -0500

    created initial trusted companies section

---
+++ components/TrustedSection.tsx
@@ -0,0 +1,40 @@
+import { cn } from "@/lib/utils";
+import Marquee from "@/components/ui/marquee";
+import { companies } from "@/constants";
+
+const TrustedCard = ({ img, name }: { img: string; name: string }) => {
+  return (
+    <div
+      className={cn(
+        "relative w-32 cursor-pointer overflow-hidden rounded-xl border p-4 items-center justify-center",
+        "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]"
+      )}>
+      <img
+        className="rounded-full"
+        width="64"
+        height="64"
+        alt={name}
+        src={img}
+      />
+    </div>
+  );
+};
+
+export function TrustedSection() {
+  return (
+    <div className="container mx-auto px-4 pb-12">
+      <h2 className="text-center text-gray-500 text-base font-semibold tracking-wider mb-4">
+        TRUSTED BY LEADING COMPANIES
+      </h2>
+      <div className="relative flex h-full max-w-5xl mx-auto flex-col items-center justify-center overflow-hidden">
+        <Marquee pauseOnHover={false} className="[--duration:20s]">
+          {companies.map((company) => (
+            <TrustedCard key={company.name} {...company} />
+          ))}
+        </Marquee>
+        <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-gray-950"></div>
+        <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-gray-950"></div>
+      </div>
+    </div>
+  );
+}
