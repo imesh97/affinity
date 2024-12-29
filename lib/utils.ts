e91417b2688a8b54ede125d65e450a3c1b29f9a9
imesh <imesh.nimsitha@gmail.ca>
2024-12-28 21:35:29 -0500

    added scroll to top function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import React from "react";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function scrollToTop(
+  e: React.MouseEvent<HTMLAnchorElement, MouseEvent>
+) {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 }
