6a758986318ef77294acfaeab2f3f004e82fea34
imesh <imesh.nimsitha@gmail.ca>
2024-12-28 20:31:35 -0500

    added trusted companies list

--- constants/index.ts
+++ constants/index.ts
@@ -5,3 +5,30 @@ export const navLinks = [
   { name: "Pricing", href: "pricing" },
   { name: "Contact", href: "contact" },
 ];
+
+export const companies = [
+  {
+    name: "Jack",
+    img: "https://avatar.vercel.sh/jack",
+  },
+  {
+    name: "Jill",
+    img: "https://avatar.vercel.sh/jill",
+  },
+  {
+    name: "John",
+    img: "https://avatar.vercel.sh/john",
+  },
+  {
+    name: "Jane",
+    img: "https://avatar.vercel.sh/jane",
+  },
+  {
+    name: "Jenny",
+    img: "https://avatar.vercel.sh/jenny",
+  },
+  {
+    name: "James",
+    img: "https://avatar.vercel.sh/james",
+  },
+];
