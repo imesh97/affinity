2ebd22dd804b0404cf326d30b67dcdbd456ae1be
imesh <imesh.nimsitha@gmail.ca>
2024-12-30 07:32:10 -0500

    changed to dark mode

--- components/ui/safari.tsx
+++ components/ui/safari.tsx
@@ -23,46 +23,30 @@ export default function Safari({
       viewBox={`0 0 ${width} ${height}`}
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
+      {...props}>
       <g clipPath="url(#path0)">
         <path
           d="M0 52H1202V741C1202 747.627 1196.63 753 1190 753H12C5.37258 753 0 747.627 0 741V52Z"
-          className="fill-[#E5E5E5] dark:fill-[#404040]"
+          className="fill-[#404040]"
         />
         <path
           fillRule="evenodd"
           clipRule="evenodd"
           d="M0 12C0 5.37258 5.37258 0 12 0H1190C1196.63 0 1202 5.37258 1202 12V52H0L0 12Z"
-          className="fill-[#E5E5E5] dark:fill-[#404040]"
+          className="fill-[#404040]"
         />
         <path
           fillRule="evenodd"
           clipRule="evenodd"
           d="M1.06738 12C1.06738 5.92487 5.99225 1 12.0674 1H1189.93C1196.01 1 1200.93 5.92487 1200.93 12V51H1.06738V12Z"
-          className="fill-white dark:fill-[#262626]"
+          className="fill-[#262626]"
         />
-        <circle
-          cx="27"
-          cy="25"
-          r="6"
-          className="fill-[#E5E5E5] dark:fill-[#404040]"
-        />
-        <circle
-          cx="47"
-          cy="25"
-          r="6"
-          className="fill-[#E5E5E5] dark:fill-[#404040]"
-        />
-        <circle
-          cx="67"
-          cy="25"
-          r="6"
-          className="fill-[#E5E5E5] dark:fill-[#404040]"
-        />
+        <circle cx="27" cy="25" r="6" className="fill-[#404040]" />
+        <circle cx="47" cy="25" r="6" className="fill-[#404040]" />
+        <circle cx="67" cy="25" r="6" className="fill-[#404040]" />
         <path
           d="M286 17C286 13.6863 288.686 11 292 11H946C949.314 11 952 13.6863 952 17V35C952 38.3137 949.314 41 946 41H292C288.686 41 286 38.3137 286 35V17Z"
-          className="fill-[#E5E5E5] dark:fill-[#404040]"
+          className="fill-[#404040]"
         />
         <g className="mix-blend-luminosity">
           <path
@@ -76,8 +60,7 @@ export default function Safari({
             y="30"
             fill="#A3A3A3"
             fontSize="12"
-            fontFamily="Arial, sans-serif"
-          >
+            fontFamily="Arial, sans-serif">
             {url}
           </text>
         </g>
@@ -147,8 +130,7 @@ export default function Safari({
             width="1200"
             height="700"
             preserveAspectRatio="xMidYMid slice"
-            clipPath="url(#roundedBottom)"
-          >
+            clipPath="url(#roundedBottom)">
             <video
               className="size-full overflow-hidden object-cover"
               src={videoSrc}
