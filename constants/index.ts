0832bb2606f1362d6a1235ffb6957fe197705600
Imesh Nimsitha <imesh@imMac.local>
2025-01-19 22:56:51 -0500

    changed Contact page to FAQs

--- constants/index.ts
+++ constants/index.ts
@@ -3,7 +3,7 @@ export const navLinks = [
   { name: "Features", href: "#features" },
   { name: "Testimonials", href: "#testimonials" },
   { name: "Pricing", href: "pricing" },
-  { name: "Contact", href: "contact" },
+  { name: "FAQs", href: "faqs" },
   { name: "Demo", href: "#demo"}
 ];
 
