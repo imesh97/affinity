c10d6e76dc567384a3838b34ea9e6d856435f579
Imesh Nimsitha <imesh.nimsitha@gmail.com>
2025-02-15 21:57:39 -0500

    added stock image for preview section

--- components/PreviewSection.tsx
+++ components/PreviewSection.tsx
@@ -4,11 +4,12 @@ export default function PreviewSection() {
   return (
     <div
       className="relative w-full max-w-5xl 2xl:max-w-6xl mx-auto px-6 md:px-12 lg:px-8 pb-20"
-      id="product">
+      id="product"
+    >
       <Safari
         url="affinity.ai"
         className="size-full border-none shadow-lg"
-        imageSrc="https://via.placeholder.com/1200x750"
+        imageSrc="/assets/cta.png"
       />
     </div>
   );
