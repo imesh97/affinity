1ddfce7b0567f7af1b87fe00c2ff3950081fe5f4
Imesh Nimsitha <imesh.nimsitha@gmail.com>
2025-03-30 00:01:28 -0400

    removed password protection

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,10 @@ import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import IntegrationsSection from "@/components/IntegrationsSection";
 import MVPSection from "@/components/MVPSection";
-import { PasswordPrompt } from "@/components/PasswordPrompt";
 import SolutionsSection from "@/components/SolutionsSection";
 import TestimonialsSection from "@/components/TestimonialsSection";
-import { cookies } from "next/headers";
 
 export default async function Home() {
-  const cookieStore = await cookies();
-  const loginCookie = cookieStore.get(process.env.PASSWORD_COOKIE!);
-  const isLoggedIn = !!loginCookie?.value;
-
-  if (!isLoggedIn) {
-    return <PasswordPrompt />;
-  }
-
   return (
     <main className="w-full overflow-x-hidden">
       <HeroSection />
