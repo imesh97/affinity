c5f4c3bf3f24375e23765bd6fc49cb36ae849291
Imesh Nimsitha <imesh.nimsitha@gmail.com>
2025-04-05 16:50:57 -0400

    better phone parsing with libphonenumber

--- lib/schemas.ts
+++ lib/schemas.ts
@@ -1,11 +1,29 @@
 import { z } from "zod";
+import parsePhoneNumberFromString from "libphonenumber-js";
+
+const phoneSchema = z
+  .string()
+  .nonempty({ message: "Please enter a valid phone number." })
+  .refine(
+    (number) => {
+      try {
+        const phoneNumber = parsePhoneNumberFromString(number, {
+          // libphonenumber parsing
+          defaultCountry: "CA",
+          extract: false,
+        });
+        return phoneNumber && phoneNumber.isValid();
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      } catch (error) {
+        return false;
+      }
+    },
+    { message: "Please enter a valid phone number." }
+  );
 
 export const BookDemoSchema = z.object({
   name: z.string().min(3, "Please enter a valid name."),
   company: z.string().min(3, "Please enter a valid company name."),
   email: z.string().email().min(4, "Please enter a valid email."),
-  phone: z
-    .string() // TODO: implement better phone number check (basic version right now)
-    .min(1, "Please enter a valid phone number.")
-    .max(15, "Please enter a valid phone number."),
+  phone: phoneSchema, // libphonenumber parsing
 });
